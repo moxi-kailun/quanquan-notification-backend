65276bd115ea7ab5349e5413f48b4004d3d0560a
Tan Kai Lun <kailun@moxi-cn.com>
2025-11-09 15:10:37 +0800

    init: Initial project setup

---
+++ src/main/java/com/moxi/quanquan/quanquan_notification_backend/QuanquanNotificationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moxi.quanquan.quanquan_notification_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanquanNotificationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanquanNotificationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moxi/quanquan/quanquan_notification_backend/QuanquanNotificationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moxi.quanquan.quanquan_notification_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanquanNotificationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
